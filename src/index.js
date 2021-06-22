c7996101a03f4716b494620a085c182f0ca42e66
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-06-22 10:44:36 -0500

    Primera version d ela validacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+/* eslint-disable linebreak-style */
 import validator from './validator.js';
 
-console.log(validator);
+// console.log(validator);
+
+function numeros(e) {
+  if (e.keyCode < 48 || e.keyCode > 57) {
+    e.returnValue = false;
+  }
+}
+
+function val() {
+  const nroTarjeta = document.getElementById('nroTarjeta').value;
+  const validador = validator.isValid(nroTarjeta);
+  const mascara = validator.maskify(nroTarjeta);
+  const franquicia = validator.franquicia(nroTarjeta);
+  console.log(franquicia);
+  document.getElementById('valor').innerHTML = validador ? 'Tu tarjeta es valida' : 'Tu tarjeta no es valida';
+  document.getElementById('maskify').innerHTML = mascara;
+}
+
+
+document.getElementById('nroTarjeta').addEventListener('keypress', (e) => { numeros(e); });
+document.getElementById('btnValidar').addEventListener('click', () => { val() });
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
+/* eslint-disable linebreak-style */
 const validator = {
-  // ...
+  isValid(nroTarjeta) {
+    // let nroTarjeta= document.getElementById("nroTarjeta").value;
+    // console.log(nroTarjeta);
+    // document.getElementById("valor").innerHTML=nroTarjeta;
+
+    const tarjetaArray = Array.from(nroTarjeta, (x) => parseInt(x));
+    // console.log(tarjetaArray);
+
+    const reverse = tarjetaArray.reverse();
+    // console.log(reverse);
+
+    let suma = 0;
+
+    for (let i = 0; i < tarjetaArray.length; i++) {
+      let element = reverse[i];
+      if (i % 2 !== 0) {
+        element *= 2;
+        if (element > 9) {
+          element -= 9; // suma de digitos
+          // element=1 + (element%10);
+        }
+      }
+      suma += element;
+    }
+    // console.log(suma);
+
+    if (suma % 10 == 0) {
+      return true;
+      //console.log("Tu tarjeta es valida");
+    }
+
+    return false;
+    // console.log("Tu tarjeta no es valida");
+  },
+
+  maskify:function(cardNumber){
+    // eslint-disable-next-line no-console
+        return cardNumber.split('').map((letter, idx) => idx < cardNumber.length - 4 ? '#' : letter).join('');
+  },    
+  
+  franquicia:function(cardNumber) {
+    if (cardNumber[0]==4) {
+      return "Visa";
+    }
+
+  
+}
 };
+
 
 export default validator;
